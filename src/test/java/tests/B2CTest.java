963f58e1e2ba6688ef55bbc606851f3a3ddbcd91
Kamini Rayna <kamini@example.com>
2026-03-18 15:42:35 +0530

    update B2CTest.java

--- src/test/java/tests/B2CTest.java
+++ src/test/java/tests/B2CTest.java
@@ -4,7 +4,6 @@ import org.testng.annotations.Test;
 
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
-import com.microsoft.playwright.options.WaitForSelectorState;
 
 import base.BaseTest;
 
